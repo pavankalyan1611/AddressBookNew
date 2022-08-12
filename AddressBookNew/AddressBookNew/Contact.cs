aa8a23d6300b4df55f0c3747f724e5f1dcedd0f6
Pavankalyan1611 <pkkoppisetti@gmail.com>
2022-08-12 11:33:06 +0530

    [pavan] address book

--- AddressBookNew/AddressBookNew/Contact.cs
+++ AddressBookNew/AddressBookNew/Contact.cs
@@ -8,5 +8,13 @@ namespace AddressBookNew
 {
     internal class Contact
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string PhNo { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Email { get; set; }
     }
 }
