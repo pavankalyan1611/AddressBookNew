008cb5c370a150d9383ff19c127fb9e8ae3365f1
Pavankalyan1611 <pkkoppisetti@gmail.com>
2022-08-16 22:52:28 +0530

    [pavan] edting exiting Contact

--- AddressBookNew/AddressBookNew/AddressBook.cs
+++ AddressBookNew/AddressBookNew/AddressBook.cs
@@ -41,18 +41,70 @@ namespace AddressBookNew
         }
         public void Display()
         {
+            Console.WriteLine("enter the first name you want to disply:");
+            string firstname = Console.ReadLine();
             foreach (Contact contact in addressBook)
             {
-                Console.WriteLine(contact.FirstName);
-                Console.WriteLine(contact.LastName);
-                Console.WriteLine(contact.Address);
-                Console.WriteLine(contact.City);
-                Console.WriteLine(contact.State);
-                Console.WriteLine(contact.PhNo);
-                Console.WriteLine(contact.Zip);
-                Console.WriteLine(contact.Email);
-
+                if (contact.FirstName.ToLower() == firstname.ToLower())
+                {
+                    Console.WriteLine(contact.FirstName);
+                    Console.WriteLine(contact.LastName);
+                    Console.WriteLine(contact.Address);
+                    Console.WriteLine(contact.PhNo);
+                    Console.WriteLine(contact.City);
+                    Console.WriteLine(contact.State);
+                    Console.WriteLine(contact.Zip);
+                    Console.WriteLine(contact.Email);
+                }
+                return;
+            }          
+                Console.WriteLine("no contact with this firstname\nthere should be min one contactin address book to display, so Add new contact");
+                AddNewContact();
+                Display();
+        }
+        public void EditContact()
+        {
+            Console.WriteLine("enter the first name you want to edit");
+            string firstname = Console.ReadLine();
+            foreach (Contact contact in addressBook)
+            {
+                if (contact.FirstName.ToLower() == firstname.ToLower())
+                {
+                    Console.WriteLine("enter which information you wnat to change");
+                    Console.WriteLine("enter 1 to edit last name,2 to change the city, 3 To state, 4 to zip,5 to Email,6 to phonenumber");
+                    int option = Convert.ToInt32(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                            Console.WriteLine("enter the lastname");
+                            contact.LastName = Console.ReadLine();
+                            break;
+                        case 2:
+                            Console.WriteLine("enter the city");
+                            contact.City = Console.ReadLine();
+                            break;
+                        case 3:
+                            Console.WriteLine("enter the State");
+                            contact.State = Console.ReadLine();
+                            break;
+                        case 4:
+                            Console.WriteLine("enter the zip");
+                            contact.Zip = Console.ReadLine();
+                            break;
+                        case 5:
+                            Console.WriteLine("enter the Email");
+                            contact.Email = Console.ReadLine();
+                            break;
+                        case 6:
+                            Console.WriteLine("enter the phonenumber");
+                            contact.PhNo = Console.ReadLine();
+                            break;
+                    }
+                    return;
+                }
             }
+            Console.WriteLine("no contact with this firstname\n Add New contact to AdderssBook:");
+            AddNewContact();
         }
     }
 }
--- AddressBookNew/AddressBookNew/Program.cs
+++ AddressBookNew/AddressBookNew/Program.cs
@@ -1,3 +1,25 @@
 ﻿using AddressBookNew;
 
-Contact contact = new Contact();
+AddressBook address = new AddressBook();
+int option = 1;
+
+while (option != 4)
+{
+    Console.WriteLine("Please enter a Num: 1- add , 2 - edit, 3 - display, 4 - exit: ");
+    option = int.Parse(Console.ReadLine().ToLower());
+
+    switch (option)
+    {
+        case 1:
+            address.AddNewContact();
+            break;
+        case 2:
+            address.EditContact();
+            break;
+        case 3:
+            address.Display();
+            break;
+        default:
+            break;
+    }
+}
